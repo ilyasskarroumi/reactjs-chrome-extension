aa5c70bccdf4fb825dbdf36bb7661d72ef8b08ca
Ilyass Karroumi <karroumi2002@gmail.com>
2023-07-11 11:16:39 +0100

    Finishing Popup

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,19 +1,34 @@
 import React from "react";
-import './popup.css'
+import './popup.css';
 
 const Popup = () => {
-    return (
-        <div className="flex justify-center bg-gray-200 items-center h-screen">
-            <div className="text-center">
-                <div className="p-4 mb-4">
-                    <h4 className="text-4xl text-blue-500">Welcome to</h4>
-                </div>
-                <div className="p-4">
-                    <img className="block" src="https://beta.mobioptions.com/static/media/argon-react.145d77702522ea432b33.png" width={200} />
-                </div>
-            </div>
+  const handleClick = () => {
+    window.open("https://play.google.com/store", "_blank");
+  };
+
+  return (
+    <div
+      className="flex justify-center bg-gray-200 items-center h-screen"
+      onClick={handleClick}
+      style={{cursor: "pointer"}}
+    >
+      <div className="text-center">
+        <div style={{display: "flex", justifyContent: "center"}}>
+          <img
+            className="block"
+            src="https://beta.mobioptions.com/static/media/argon-react.145d77702522ea432b33.png"
+            width={200}
+            alt="Logo"
+          />
+        </div>
+        <div className="mt-4">
+          <p className="text-sm text-blue-600">
+            Click anywhere to go to Google Play Store
+          </p>
+        </div>
       </div>
-    )
+    </div>
+  );
 };
 
 export default Popup;
