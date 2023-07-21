8e9b9a68190dd2cee34ad323cef4ad14bd42a70a
Ilyass Karroumi <karroumi2002@gmail.com>
2023-07-21 19:43:08 +0100

    Done

--- webpack.common.js
+++ webpack.common.js
@@ -8,10 +8,8 @@ const autoprefixer = require('autoprefixer')
 module.exports = {
     entry: {
         popup: path.resolve('src/popup/index.tsx'),
-        options: path.resolve('src/options/index.tsx'),
         background: path.resolve('src/background/background.ts'),
         contentScript: path.resolve('src/contentScript/contentScript.tsx'),
-        newTab: path.resolve('src/tabs/index.tsx'),
     },
     module: {
         rules: [
@@ -58,9 +56,7 @@ module.exports = {
             }]
         }),
         ...getHtmlPlugins([
-            'popup',
-            'options',
-            'newTab'
+            'popup'
         ])
     ],
     resolve: {
